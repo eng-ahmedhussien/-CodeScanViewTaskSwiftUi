9aee8b2ec61988964668734bbc7685d6fc3c6b30
ahmed hussien <33827384+eng-ahmedhussien@users.noreply.github.com>
2023-08-01 13:01:30 +0300

    Add files via upload

---
+++ BarcodeImageView/BarcodeImageView.swift
@@ -0,0 +1,51 @@
+//
+//  BarcodeImageView.swift
+//  AlDwaaNewApp
+//
+//  Created by ahmed hussien on 31/05/2023.
+//
+
+import SwiftUI
+
+struct BarcodeImageView: View {
+    
+    @State var text : String
+    
+    var body: some View {
+        
+        VStack(spacing:20){
+            if let image = generateBarcodeData(from: self.text){
+                Image(uiImage: UIImage(data: image)!)
+                    .resizable()
+                    .scaledToFit()
+            }else{
+                Image(systemName: "camera.metering.unknown")
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            Text(text)
+        }
+        .frame(width: 350, height: 200, alignment: .center)
+        .background(.gray)
+        .cornerRadius(20)
+    }
+    
+    func generateBarcodeData(from string: String) -> Data?{
+        let data = string.data(using: String.Encoding.ascii)
+        if let filter = CIFilter(name: "CICode128BarcodeGenerator"){
+            filter.setValue(data, forKey: "inputMessage")
+            if let image = filter.outputImage{
+                let uiimage = UIImage(ciImage: image)
+                return uiimage.pngData()!
+            }
+        }
+        return nil
+    }
+}
+
+struct BarcodeGenerator_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeImageView(text: "12345")
+    }
+}
---
+++ CodeView/CodeScanView.swift
@@ -0,0 +1,64 @@
+//
+//  CodeScannerView.swift
+//  AlDwaaNewApp
+//
+//  Created by ahmed hussien on 25/05/2023.
+//
+
+import SwiftUI
+import CodeScanner
+import AVFoundation
+
+
+struct CodeScanView: View {
+    @Binding var scaneResult:String?
+    @Environment(\.dismiss) var dismiss
+    @State var animted = false
+    let codeTypes : [AVMetadataObject.ObjectType] = [.qr,.code39,.code93,.code128,.ean8,.ean13,.upce,.itf14]
+
+    var body: some View {
+        
+        CodeScannerView(codeTypes: codeTypes ,
+                        completion:self.handleScan )
+        .overlay(
+            ZStack {
+                Rectangle()
+                    .fill(Color.black.opacity(0.5))
+                    .edgesIgnoringSafeArea(.all)
+                Rectangle()
+                    .stroke(Color.yellow, lineWidth: 2)
+                    .frame(width: 200,height: 200)
+                    .scaleEffect(animted ?  1.2 : 1)
+            }
+        )
+        .onAppear {
+            addanimtion()
+        }
+    }
+    
+    func addanimtion(){
+        withAnimation(Animation.easeOut(duration: 1.0).repeatForever()) {
+            animted.toggle()
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        
+            self.animted = false
+            switch result {
+            case .success(let res):
+                scaneResult = res.string
+                dismiss()
+            case .failure(_):
+                scaneResult = nil
+                dismiss()
+            }
+        }
+}
+struct CodeScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CodeScanView(scaneResult: .constant(""))
+    }
+}
+
+
